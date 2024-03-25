447c8e3d576450586efea61374115928d4a7caeb
JoeWilliamson32 <119574925+JoeWilliamson32@users.noreply.github.com>
2024-03-25 20:57:53 +0000

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+
+st.title('Home')
+
+st.subheader("Welcome to @JoeW_32's web app")
+
+st.divider()
+
+st.markdown(
+"""
+Only developed for the Premier League 23/24 season as of the minute, but plan to add more in due course. Current tools are as follows:
+- **Player Profiles:** Compare a player's percentile rank to others playing in their position. 
+- **Player Similarity:** Use PCA to find the most similar players. 
+- **Match Analysis:** Match dashboards of games. 
+- **Team Profiles:** Compare team performance.  
+
+
+Please follow https://twitter.com/JoeW__32__Bot on Twitter.
+
+"""
+)
