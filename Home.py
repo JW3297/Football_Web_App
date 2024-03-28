34747473cca2dfb81a2bac57476fec673bade5db
JoeWilliamson32 <119574925+JoeWilliamson32@users.noreply.github.com>
2024-03-28 16:18:30 +0000

    Removed Match Analysis - Not needed

--- Home.py
+++ Home.py
@@ -11,7 +11,6 @@ st.markdown(
 Only developed for the Premier League 23/24 season as of the minute, but plan to add more in due course. Current tools are as follows:
 - **Player Profiles:** Compare a player's percentile rank to others playing in their position. 
 - **Player Similarity:** Use PCA to find the most similar players. 
-- **Match Analysis:** Match dashboards of games. 
 - **Team Profiles:** Compare team performance.  
 
 
