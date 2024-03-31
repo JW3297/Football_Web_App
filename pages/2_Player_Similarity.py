2e2a20e4db63c92aec0a8d2003005cb795637384
JoeWilliamson32 <119574925+JoeWilliamson32@users.noreply.github.com>
2024-03-31 18:53:28 +0100

    Update 2_Player_Similarity.py

--- pages/2_Player_Similarity.py
+++ pages/2_Player_Similarity.py
@@ -1,6 +1,10 @@
+# General
 import pandas as pd
 import numpy as np
 import io
+
+# Plotting
+from mplsoccer import Radar, grid
 
 # ML
 from sklearn import preprocessing
@@ -111,6 +115,198 @@ def df_player(df_pos, df_ranks, name):
     return df_player_rank, df_player_vals, mins
 
 
+def plotter(df_player_rank1, df_player_vals1, name1, pos, mins1,
+            df_player_rank2, df_player_vals2, name2, mins2):
+    
+    if pos == 'Center Mid':
+        cols = [
+                'Passes Attempted', 'Pass Completion Rate', 'Forward Passes','Long Passes', 'Progressive Passes',
+                'npxG', 'xA', 'Shots',
+                'Tackles', 'Interceptions', 'Fouls','Aerials',
+                'Touches', 'xT Received','Progressive Carries'
+               ]
+        df_player_rank1 = df_player_rank1[cols]
+        df_player_vals1 = df_player_vals1[cols]
+        df_player_rank2 = df_player_rank2[cols]
+        df_player_vals2 = df_player_vals2[cols]
+        
+        fields = [
+                'Passes \nAttempted', 'Pass \nCompletion Rate', 'Forward \nPasses', 'Long \nPasses', 'Progressive \nPasses',
+                'npxG', 'xA', 'Shots',
+                'Tackles', 'Interceptions', 'Fouls','Aerials',
+                'Touches', 'xT Received','Progressive \nCarries'
+               ]   
+        
+    
+    elif pos == 'Attacking Mid/Winger':
+        cols = [
+                'Pass Completion Rate', 'Forward Passes','Progressive Passes','Box Passes',
+                'npxG', 'xA', 'Shots',
+                'Tackles', 'Fouls', 'Recoveries',
+                'Box Touches', 'Final 1/3 Touches', 'xT Received','Progressive Carries', 'Successful Take Ons'
+               ]
+        df_player_rank1 = df_player_rank1[cols]
+        df_player_vals1 = df_player_vals1[cols]
+        df_player_rank2 = df_player_rank2[cols]
+        df_player_vals2 = df_player_vals2[cols]
+        
+        
+        fields = [
+                'Pass \nCompletion Rate', 'Forward \nPasses','Progressive \nPasses','Box \nPasses',
+                'npxG', 'xA', 'Shots',
+                'Tackles', 'Fouls', 'Recoveries',
+                'Box \nTouches', 'Final 1/3 \nTouches', 'xT Received','Progressive \nCarries', 'Successful \nTake Ons'
+               ]   
+        
+        
+        
+    elif pos == 'Full Back':
+        cols = [
+                'Pass Completion Rate', 'Forward Passes','Progressive Passes',
+                'npxG', 'xA',
+                'Tackles', 'Interceptions', 'Fouls', 'Aerials', 'Recoveries',
+                'Touches', 'Final 1/3 Touches', 'Progressive Carries', 'Successful Take Ons'
+               ]
+        
+        df_player_rank1 = df_player_rank1[cols]
+        df_player_vals1 = df_player_vals1[cols]
+        df_player_rank2 = df_player_rank2[cols]
+        df_player_vals2 = df_player_vals2[cols]
+        
+        
+        fields = [
+                'Pass \nCompletion Rate', 'Forward \nPasses','Progressive \nPasses',
+                'npxG', 'xA',
+                'Tackles', 'Interceptions', 'Fouls', 'Aerials', 'Recoveries',
+                'Touches', 'Final 1/3 \nTouches', 'Progressive \nCarries', 'Successful \nTake Ons'
+               ]
+        
+        
+    elif pos == 'Forward':
+        cols = [
+                'Pass Completion Rate', 'Progressive Passes', 'Box Passes',
+                'npxG', 'xA', 'Shots', 
+                'Fouls', 'Aerials', 'Recoveries',
+                'Box Touches', 'Final 1/3 Touches', 'xT Received','Progressive Carries', 'Successful Take Ons'
+               ]
+        
+        df_player_rank1 = df_player_rank1[cols]
+        df_player_vals1 = df_player_vals1[cols]
+        df_player_rank2 = df_player_rank2[cols]
+        df_player_vals2 = df_player_vals2[cols]
+        
+        fields = [
+                'Pass \nCompletion Rate', 'Progressive \nPasses', 'Box \nPasses',
+                'npxG', 'xA', 'Shots',
+                'Fouls', 'Aerials', 'Recoveries',
+                'Box \nTouches', 'Final 1/3 \nTouches', 'xT Received','Progressive \nCarries', 'Successful \nTake Ons'
+               ]
+
+
+    elif pos == 'Center Back':
+        cols = [
+                'Passes Attempted', 'Pass Completion Rate', 'Long Passes', 'Progressive Passes',
+                'npxG', 'xA',
+                'Tackles', 'Interceptions', 'Fouls', 'Aerials', 'Recoveries',
+                'Touches', 'Progressive Carries'
+               ]
+        
+        df_player_rank1 = df_player_rank1[cols]
+        df_player_vals1 = df_player_vals1[cols]
+        df_player_rank2 = df_player_rank2[cols]
+        df_player_vals2 = df_player_vals2[cols]
+        
+        
+        fields = [
+                'Passes \nAttempted', 'Pass \nCompletion Rate', 'Long \nPasses', 'Progressive \nPasses',
+                'npxG', 'xA',
+                'Tackles', 'Interceptions', 'Fouls', 'Aerials', 'Recoveries',
+                'Touches', 'Progressive \nCarries',
+               ]
+        
+    
+    fields = [field.upper() for field in fields]
+    
+    values = df_player_rank1.loc[0, :].values.flatten().tolist()
+    values = [round(val,2) for val in values]
+    values2 = df_player_vals1.loc[0, :].values.flatten().tolist()
+    values2 = [round(val,2) for val in values2]
+    
+    
+    values3 = df_player_rank2.loc[0, :].values.flatten().tolist()
+    values3 = [round(val,2) for val in values3]
+    values4 = df_player_vals2.loc[0, :].values.flatten().tolist()
+    values4 = [round(val,2) for val in values4]
+    
+    fig, axs = grid(figheight=14, grid_height=0.875, title_height=0.1, endnote_height=0.025,
+                title_space=0, endnote_space=0, grid_key='radar', axis=False)
+    
+    
+    low = [0] * len(fields)
+    hi = [1] * len(fields)
+    
+    radar = Radar(fields, low, hi,
+                  num_rings=4, 
+                  ring_width=1, 
+                  center_circle_radius=1)
+
+
+    radar.setup_axis(ax=axs['radar'], facecolor='#2B2B2B')
+
+    rings_inner = radar.draw_circles(ax=axs['radar'], facecolor='#2B2B2B', edgecolor='white', alpha=0.4, lw=1.5)
+
+    radar_output = radar.draw_radar_compare(values, values3,  ax=axs['radar'],
+                                            kwargs_radar={'facecolor': '#1A78CF', 'alpha':0.55},
+                                            kwargs_compare={'facecolor': '#D70232', 'alpha': 0.6})
+
+    radar_poly, radar_poly2, vertices1, vertices2 = radar_output
+
+    col_labels = radar.draw_param_labels(ax=axs['radar'],color="white", fontsize=18, fontname = 'Sans Serif')
+
+    rot = 360
+    for i in range(len(vertices1)):
+        rot = 360-((360/len(cols))*i)
+        if rot in range(90, 270):
+            rot = rot - 180 
+
+        x,y = vertices1[i]
+        val = values2[i]
+        axs['radar'].annotate(xy = (x,y), s = val, rotation=rot,
+                              bbox=dict(facecolor= '#1A78CF', edgecolor='white', boxstyle='round', alpha=1), 
+                              color='white', fontname = 'Sans Serif', fontsize = 12)
+
+
+    rot = 360
+    for i in range(len(vertices2)):
+        rot = 360-((360/len(cols))*i)
+        if rot in range(90, 270):
+            rot = rot - 180 
+
+        x,y = vertices2[i]
+        val = values4[i]
+        axs['radar'].annotate(xy = (x,y), s = val, rotation=rot,
+                              bbox=dict(facecolor= '#D70232', edgecolor='white', boxstyle='round', alpha=1), 
+                              color='white', fontname = 'Sans Serif', fontsize = 12)
+
+    title1_text = axs['title'].text(0.02, 0.85, name1.upper(), fontsize=25, fontname = 'Sans Serif',
+                                ha='left', va='center', color='white')
+    title1_text = axs['title'].text(0.02, 0.6, str(mins1) + ' MINS', fontsize=17, fontname = 'Sans Serif',
+                                ha='left', va='center', color='white')
+    title3_text = axs['title'].text(0.98, 0.85, name2.upper(), fontsize=25, fontname = 'Sans Serif',
+                                    ha='right', va='center', color='white')
+    title4_text = axs['title'].text(0.98, 0.6, str(mins2) + ' MINS', fontsize=17, fontname = 'Sans Serif',
+                                    ha='right', va='center', color='white')
+    axs['title'].axhline(y = 0.4, xmin = 0.02, xmax = 0.5, color='#1A78CF', lw=3) 
+    axs['title'].axhline(y = 0.4, xmin = 0.5, xmax = 0.98, color='#D70232', lw=3) 
+
+    endnote_text = axs['endnote'].text(0.8, 0.5, 'TEMPLATE: ' + position + '\n\nCREATED BY @JoeW_32', fontsize=12,
+                                       fontname = 'Sans Serif', ha='left', va='center', color='white')
+
+    fig.set_facecolor('#2B2B2B')
+
+    
+    return fig
+
 
 
 st.title('Player Similarity Search')
@@ -170,6 +366,24 @@ if generate:
     )
 
     df_pos2, df_ranks2 = data_prep_posMins(df, position)
-    df_rank2, df_vals2, mins2 = df_player(df_pos2, df_ranks2, player2)  
+    df_rank2, df_vals2, mins2 = df_player(df_pos2, df_ranks2, player2) 
 
-    
+    plot = st.button('Plot Comparison')
+
+    if plot:
+
+        fig = plotter(df_rank1, df_vals1, player1, position, mins1,
+                      df_rank2, df_vals2, player2, mins2) 
+        
+        st.write(fig)
+
+        b = io.BytesIO()
+        fig.savefig(b, format='png', bbox_inches="tight")
+
+        btn = st.download_button(
+            label="Download Plot",
+            data=b,
+            file_name= player1.replace(' ', '') + "vs" + player2.replace(' ', '') + "Plot.png",
+            mime="image/png"
+            )
+
