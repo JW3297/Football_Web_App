8870f6c73e1a7742f361dbcfda7236bbc1a5a0fe
JoeWilliamson32 <119574925+JoeWilliamson32@users.noreply.github.com>
2024-03-31 21:05:12 +0100

    Update 2_Player_Similarity.py

--- pages/2_Player_Similarity.py
+++ pages/2_Player_Similarity.py
@@ -262,8 +262,8 @@ def plotter(df_player_rank1, df_player_vals1, name1, pos, mins1,
 
     rot = 360
     for i in range(len(vertices1)):
-        rot = 360-((360/len(fields))*i)
-        if rot in range(135, 225):
+        rot = round(360-((360/len(cols))*i),0)
+        if rot in range(90, 270):
             rot = rot - 180 
 
         x,y = vertices1[i]
@@ -275,7 +275,7 @@ def plotter(df_player_rank1, df_player_vals1, name1, pos, mins1,
 
     rot = 360
     for i in range(len(vertices2)):
-        rot = 360-((360/len(fields))*i)
+        rot = round(360-((360/len(cols))*i),0)
         if rot in range(90, 270):
             rot = rot - 180 
 
