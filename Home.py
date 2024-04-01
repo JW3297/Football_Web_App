c319b09ae1a0aabe738e5bcf7fc8433768e37353
JoeWilliamson32 <119574925+JoeWilliamson32@users.noreply.github.com>
2024-04-01 20:21:51 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ st.markdown(
 """
 Only developed for the Premier League 23/24 season as of the minute, but plan to add more in due course. Current tools are as follows:
 - **Player Profiles:** Compare a player's percentile rank to others playing in their position. 
-- **Player Comparison:** Compare a player's percentile rank another specific player playing in their position. 
+- **Player Comparison:** Compare a player's percentile rank to another specific player playing in their position. 
 - **Player Similarity:** Use PCA to find the most similar players. 
 - **Team Profiles:** Compare team performance.  
 
